f2fc0c287a5816946115d69331ac1336bb84f352
EC2 Default User <nagaofujie@users.noreply.github.com>
2022-07-31 05:35:40 +0000

    productionからconfig.web_console.whitelisted_ipsを削除

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,6 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "37dbac9f7a4d4eaa89f733c03f0d98ce.vfs.cloud9.ap-northeast-1.amazonaws.com"
-
+  config.web_console.whitelisted_ips = '106.154.145.39'
 end
 
