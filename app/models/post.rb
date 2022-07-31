532bf1a17c526f2bda50bff2c5d088586ccc53d1
EC2 Default User <nagaofujie@users.noreply.github.com>
2022-07-31 06:42:45 +0000

    name

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,19 @@
 class Post < ApplicationRecord
     has_one_attached :image
-    
+
     belongs_to :customer
     has_many :favorites, dependent: :destroy
     has_many :comments, dependent: :destroy
     has_many :post_tags, dependent: :destroy
     has_many :tags, through: :post_tags
-    
+
     validates :title,
         length: { minimum: 1, maximun: 25}, presence: true
     validates :introduction,
         length: { minimum: 1, maximum: 140}, presence: true
     validates :image,
         presence: true
-    
+
     def favorited?(customer)
        favorites.where(customer_id: customer.id).exists?
     end
@@ -29,26 +29,26 @@ class Post < ApplicationRecord
         end
         image.variant(resize_to_limit: [width, height]).processed
     end
-     
+
  def save_tag(sent_tags)
-        current_tags = self.tags.pluck(:tag_list) unless self.tags.nil?
+        current_tags = self.tags.pluck(:name) unless self.tags.nil?
         old_tags = current_tags - current_tags
         new_tags = sent_tags - current_tags
-         
+
     old_tags.each do |old|
         self.tags.delete　Tag.find_by(name: old)
     end
-    
+
     new_tags.each do |new|
         new_post_tag = Tag.find_or_create_by(name: new)
         self.tags << new_post_tag
     end
  end
- 
+
 def self.search(keyword)
   where(["title like? OR introduction like?", "%#{keyword}%", "%#{keyword}%"])
 end
-    
- 
+
+
 
 end
