7cf815594161f111d1b22104644375d7e33e3d1d
EC2 Default User <nagaofujie@users.noreply.github.com>
2022-07-31 06:33:45 +0000

    tag_list=>nameに変更

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,63 +1,63 @@
 class Public::PostsController < ApplicationController
 before_action :authenticate_customer!, only: [:create, :edit, :update, :destroy, :show]
 
-    def search_tag 
+    def search_tag
         @tag_list = Tag.all
         @tag = Tag.find(params[:tag_id])
         @posts = @tag.posts.page(params[:id]).per(10)
     end
-    
+
     def search
        @result_posts = Post.search(params[:keyword])
        @tag_list = Tag.all
        @keyword = params[:keyword]
        render 'index'
     end
-    
+
     def myposts
         @posts = current_customer.posts.page(params[:page]).per(5)
         @customer = current_customer
     end
-    
-    
+
+
     def create
         @post = Post.new(post_params)
         @post.customer_id = current_customer.id
-        tag_list = params[:tag_list].split(nil) 
+        tag_list = params[:name].split(nil)
      if @post.save
-        @post.save_tag(tag_list)        
+        @post.save_tag(tag_list)
         redirect_to posts_path
-     else 
+     else
         render :new
      end
     end
-    
+
     def new
         @post = Post.new
     end
-    
+
     def index
         @posts = Post.page(params[:page]).per(5)
         @tag_list = Tag.page(params[:page]).per(20)
     end
-    
+
     def show
         @post = Post.find(params[:id])
         @tags = @post.tags
         @comment = Comment.new
         @comments = @post.comments.page(params[:page]).per(6)
-        
+
     end
-    
+
     def edit
         @post = Post.find(params[:id])
         @tag_list = @post.tags.pluck(:name).join(" ")
     end
-    
+
     def update
         @post = Post.find(params[:id])
-        tag_list = params[:post][:tag_list].split(" ")
-    
+        tag_list = params[:post][:name].split(" ")
+
         if @post.update(post_params)
                 @old_relations = PostTag.where(post_id: @post.id)
                 @old_relations.each do |relation|
@@ -71,7 +71,7 @@ before_action :authenticate_customer!, only: [:create, :edit, :update, :destroy,
             redirect_to post_path(@post.id)
         end
     end
-    
+
     def destroy
         @post = Post.find(params[:id])
         @post.destroy
@@ -79,7 +79,7 @@ before_action :authenticate_customer!, only: [:create, :edit, :update, :destroy,
     end
 
     private
-    
+
     def post_params
       params.require(:post).permit(:image, :title, :introduction)
     end
